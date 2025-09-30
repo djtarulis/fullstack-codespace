342f6f091316dc5a5856c00fc08e440eef215f34
Daniel <104313625+djtarulis@users.noreply.github.com>
2025-09-30 03:53:55 +0000

    chore: build first api route and enable CORS

--- backend/src/db/init.js
+++ backend/src/db/init.js
@@ -1,5 +1,7 @@
 import mongoose from 'mongoose'
+import dotenv from 'dotenv'
 
+dotenv.config()
 export function initDatabase() {
   const DATABASE_URL = process.env.DATABASE_URL
   mongoose.connection.on('open', () => {
--- backend/src/example.js
+++ backend/src/example.js
@@ -1,20 +1,17 @@
 import { initDatabase } from './db/init.js'
+
 import { Post } from './db/models/post.js'
 
 await initDatabase()
 
 const post = new Post({
-  title: 'Hello Mongoose!',
-  author: 'Daniel Tarulis',
-  contents: 'This post is stored in a MongoDB database using Mongoose.',
+  title: 'Hello Again Mongoose!',
+  author: 'Huseyin Ergin',
+  contents: 'This test post is stored in a MongoDB database using Mongoose.',
   tags: ['mongoose', 'mongodb'],
 })
 
-const createdPost = await post.save()
-
-await Post.findByIdAndUpdate(createdPost._id, {
-  $set: { title: 'Hello again, Mongoose!' },
-})
+await post.save()
 
 const posts = await Post.find()
 console.log(posts)
