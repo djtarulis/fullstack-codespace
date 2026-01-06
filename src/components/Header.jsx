d74cfdec0d1631f94be7b7d500ccf45f2ccaea6a
Daniel <104313625+djtarulis@users.noreply.github.com>
2026-01-06 16:19:17 +0000

    feat: implement chat feature

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,24 +2,29 @@ import { Link } from 'react-router-dom'
 import { jwtDecode } from 'jwt-decode'
 import { useAuth } from '../contexts/AuthContext.jsx'
 import { User } from './User.jsx'
+import { useSocket } from '../contexts/SocketIOContext.jsx'
 
 export function Header() {
   const [token, setToken] = useAuth()
+  const { socket } = useSocket()
+  const handleLogout = () => {
+    socket.disconnect()
+    setToken(null)
+  }
   if (token) {
     const { sub } = jwtDecode(token)
     return (
       <div>
-        <h1>Welcome To My Blog!</h1>
+        <h1>Welcome to My Blog!</h1>
         Logged in as <User id={sub} />
         <br />
-        <button onClick={() => setToken(null)}>Logout</button>
+        <button onClick={handleLogout}>Logout</button>
       </div>
     )
   }
-
   return (
     <div>
-      <h1>Welcome To My Blog!</h1>
+      <h1>Welcome to My Blog!</h1>
       <Link to='/login'>Log In</Link> | <Link to='/signup'>Sign Up</Link>
     </div>
   )
